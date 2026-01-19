c29d4d2bf2b735b4fb0ba7d2885966d1b989900e
Urgesa Jemal (under her dua)-UJ <urgesaofficial@gmail.com>
2026-01-19 17:59:10 +0300

    Update home_screen.dart

--- lib/lib/screens/home_screen.dart
+++ lib/lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'upload_screen.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,11 +8,23 @@ class HomeScreen extends StatelessWidget {
         title: Text('Agrigrade AI'),
       ),
       body: Center(
-        child: Text(
-          'Welcome to Agrigrade AI!',
-          style: TextStyle(fontSize: 20),
-        ),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'Welcome to Agrigrade AI!',
+        style: TextStyle(fontSize: 20),
       ),
-    );
-  }
-}
+      SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => UploadScreen()),
+          );
+        },
+        child: Text('Upload Crop Image'),
+      ),
+    ],
+  ),
+),
